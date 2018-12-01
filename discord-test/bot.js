26ae6c7225d0e0e11841394d787b0dbe73323dda
kitson <tkitson@udel.edu>
2018-12-01 09:47:10 -0500

    intial working version of say: crosses out message if language not spoken by the recipient

--- discord-test/bot.js
+++ discord-test/bot.js
@@ -74,19 +74,62 @@ bot.add_lang = function(userID, lang) {
         user.langs = new Set();
     }
 
-    user.langs.add(lang);
+    user.langs.add(lang.toLowerCase());
 }
 
-//sets the users current language
+//sets the user's current language
 bot.set_lang = function(userID, lang) {
+    lang = lang.toLowerCase();
     user = this.users[userID];
     logger.info(user.username + ' default language set to ' + lang);
+    user.cur_lang = lang    
+}
 
+//lists the user's current set of languages
+bot.list_langs = function(userID, lang) {
+    user = this.users[userID];
     
+    if (user.langs === undefined) {
+        this.sendMessage({
+            to: userID,
+            message: 'No languages listed for ' + user.username
+        }); 
+    } else {
+        this.sendMessage({
+            to: userID,
+            message: 'Languages listed for ' + user.username + ': '
+                     + Array.from(user.langs).join(', ')
+        });
+    }
 }
 
 bot.say = function(userID, msg) {
-    
+    sender = this.users[userID];
+    lang = sender.cur_lang;
+    if (lang === undefined) {
+        this.sendMessage({
+            to: userID,
+            message: 'Please set a language to speak in\n'
+                     + '[Usage: !lang set <language>]'
+        });
+    } else {
+        for (var recip_id in this.users) {
+            recipient = this.users[recip_id];
+            
+            if (recipient.langs !== undefined 
+                && recipient.langs.has(lang)) {
+                this.sendMessage({
+                    to: recip_id,
+                    message: sender.username + ': ' + msg
+                });
+            } else {
+                this.sendMessage({
+                    to: recip_id,
+                    message: sender.username + ': ~~' + msg + '~~'
+                });
+            }
+        }
+    }
 }
 
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -112,19 +155,29 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 						break;
 
             case 'lang':
-                switch(args[0]) {
+                //take the subcommand out of the set of args
+                subcmd = args[0];
+                args = args.splice(1);
+                switch(subcmd) {
                     case 'add':
-                        //take add out of the set of args
-                        args = args.splice(1);
                         for (var i in args) {
                             bot.add_lang(userID, args[i]);
                         }
+                        break;
+                    
+                    case 'set':
+                        bot.set_lang(userID, args[0]);
+                        break;
+
+                    case 'list':
+                        bot.list_langs(userID);
                         break;
                 }
                 break;
             
             case 'say':
             case '':
+                message = args.join(' ');
                 bot.say(userID, message);
                 break;
             // Just add any case commands if you want to..
